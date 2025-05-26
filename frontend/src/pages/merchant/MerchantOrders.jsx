7c9c803c7496993fc008cb46f119c74287545300
nevibulatao <ntbulatao@up.edu.ph>
2025-05-26 15:54:19 +0800

    fix: fixed merge errors

---
+++ frontend/src/pages/merchant/MerchantOrders.jsx
@@ -0,0 +1,12 @@
+import Navbar from '../../components/NavBar';
+
+const MerchantOrders = () => {
+    return (
+      <div>
+        <Navbar />
+        
+      </div>
+    );
+};
+
+export default MerchantOrders;
